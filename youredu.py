0b6a871d2025dc5316ef8a19d1dd84468d7664eb
Shishir Modi <shishirmodi0815@gmail.com>
2020-10-14 15:03:18 +0530

    Corrected Indentation
    Corrected indentation which was probably making the code loop back

--- youredu.py
+++ youredu.py
@@ -63,69 +63,69 @@ if __name__=='__main__':
             continue
 
 if "bye" in statement or "stop" in statement:
-  speak('Glad to help you! I will shut down now')
-  print('Glad to help you! I will shut down now')
-  time.sleep(2)
-  sys.exit()
+    speak('Glad to help you! I will shut down now')
+    print('Glad to help you! I will shut down now')
+    time.sleep(2)
+    sys.exit()
 
 elif 'wikipedia' in statement:
-  speak('Searching Wikipedia...')
-  statement =statement.replace("wikipedia", "")
-  results = wikipedia.summary(statement, sentences=3)
-  speak("According to Wikipedia")
-  print(results)
-  speak(results)
+    speak('Searching Wikipedia...')
+    statement =statement.replace("wikipedia", "")
+    results = wikipedia.summary(statement, sentences=3)
+    speak("According to Wikipedia")
+    print(results)
+    speak(results)
 
 elif 'open youtube' in statement:
-  webbrowser.open_new_tab("https://www.youtube.com")
-  speak("Youtube is open now")
-  time.sleep(5)
+    webbrowser.open_new_tab("https://www.youtube.com")
+    speak("Youtube is open now")
+    time.sleep(5)
 
 elif 'open google' in statement:
-  webbrowser.open_new_tab("https://www.google.com")
-  speak("Google is open now")
-  time.sleep(5)
+    webbrowser.open_new_tab("https://www.google.com")
+    speak("Google is open now")
+    time.sleep(5)
 
 elif 'open gmail' in statement:
-  webbrowser.open_new_tab("gmail.com")
-  speak("Gmail is open now")
-  time.sleep(5)
+    webbrowser.open_new_tab("gmail.com")
+    speak("Gmail is open now")
+    time.sleep(5)
 
 elif 'time' in statement:
-  strTime=datetime.datetime.now().strftime("%H:%M:%S")
-  speak(f"the time is {strTime}")
+    strTime=datetime.datetime.now().strftime("%H:%M:%S")
+    speak(f"the time is {strTime}")
 
 elif 'news' in statement:
-  news = webbrowser.open_new_tab("https://bbc.com")
-  speak('Here are some headlines from the BBC news, Happy reading!')
-  time.sleep(6)
+    news = webbrowser.open_new_tab("https://bbc.com")
+    speak('Here are some headlines from the BBC news, Happy reading!')
+    time.sleep(6)
 
 elif 'search'  in statement:
-  statement = statement.replace("search", "")
-  webbrowser.open_new_tab(statement)
-  time.sleep(5)
+    statement = statement.replace("search", "")
+    webbrowser.open_new_tab(statement)
+    time.sleep(5)
 
 elif 'what is' in statement:
-  speak('I can get you answers for computational and geographical problems. Please ask your question now')
-  question=takeCommand()
-  client = wolframalpha.Client('6QKTYA-66TGTQE6TX')
-  res = client.query(question)
-  answer = next(res.results).text
-  speak(answer)
-  print(answer)
+    speak('I can get you answers for computational and geographical problems. Please ask your question now')
+    question=takeCommand()
+    client = wolframalpha.Client('6QKTYA-66TGTQE6TX')
+    res = client.query(question)
+    answer = next(res.results).text
+    speak(answer)
+    print(answer)
 
 elif 'who are you' in statement or 'what can you do' in statement:
-  speak('I am your personal productivity assistant YourEdu. I am programmed to minor tasks like'
-  'opening youtube,google chrome, gmail and stackoverflow ,predict time,search wikipedia' 
-  'get top headline news from BBC and you can ask me computational or geographical questions too!')
+    speak('I am your personal productivity assistant YourEdu. I am programmed to minor tasks like'
+      'opening youtube,google chrome, gmail and stackoverflow ,predict time,search wikipedia' 
+      'get top headline news from BBC and you can ask me computational or geographical questions too!')
 
 elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
-  speak("I was built by Shishir Modi")
-  print("I was built by Shishir Modi")
+    speak("I was built by Shishir Modi")
+    print("I was built by Shishir Modi")
 
 elif "log off" in statement or "sign out" in statement:
-  speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
-  time.sleep(10)
-  subprocess.call(["shutdown", "/l"])
+    speak("Ok , your pc will log off in 10 sec make sure you exit from all applications")
+    time.sleep(10)
+    subprocess.call(["shutdown", "/l"])
 
 time.sleep(3)
